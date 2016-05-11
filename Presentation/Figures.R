672640092ab7601d4e4cf04edea08aed90b02821
EmiliaSicari <e.sicari@mpp.hertie-school.org>
2016-05-11 12:16:03 +0200

    eliminating doble paper and updating title

--- Presentation/Figures.R
+++ Presentation/Figures.R
@@ -13,14 +13,12 @@ try(source("/Users/rafalopezv/Dropbox/R/Final_assignment/Preliminary_analyses.R"
 #Loading packages
 library(ggplot2)
 library(scales)
-library(ggplot2)
 
 #Creating the first figure: trend in number of cars
 CARS <- ggplot2::ggplot(data.final, aes(x=data.final$date, y=data.final$car.u.pop)) +
   geom_line(aes(group=1), colour="#3399FF") + 
   theme_classic()
 
-CARS
 #Changing the name of the variables in the y axis
 CARS <- CARS + labs(x = "years", y = "cars per 100 people")
 
